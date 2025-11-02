ad0dd016055bf7ac93df63d8017a43b559e0eef1
Aleck D. Holly <aleckdavidholly@gmail.com>
2025-11-02 14:55:20 -0500

    Initial Commit

---
+++ TestNodeJS/TestNodeJSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestNodeJSApp.swift
+//  TestNodeJS
+//
+//  Created by Aleck David Holly on 2025-11-02.
+//
+
+import SwiftUI
+
+@main
+struct TestNodeJSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
